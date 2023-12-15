eb137a91d9f858064a68f42772adb4bdf69fb806
Naveen Kumar <nvnkr09@gmail.com>
2023-12-15 17:57:25 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const weatherInput = { temperature: 25, conditions: "Sunny" };
+  const temperatureThreshold = 20;
+
+  const isTempHigh = weatherInput.temperature > temperatureThreshold;
+
+  const temperatureStyle = {
+    color: isTempHigh ? 'red' : 'blue'
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p style={temperatureStyle}>{weatherInput.temperature}°C</p>
+      <p style={temperatureStyle}>{weatherInput.conditions}</p>
     </div>
   )
 }
