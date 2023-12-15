4561eab8d10780acfeb79fea33bdd1c35a81174f
Naveen Kumar <nvnkr09@gmail.com>
2023-12-15 18:14:42 +0530

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
-
   const weatherInput = { temperature: 25, conditions: "Sunny" };
   const temperatureThreshold = 20;
 
   const isTempHigh = weatherInput.temperature > temperatureThreshold;
 
   const temperatureStyle = {
-    color: isTempHigh ? 'red' : 'blue'
-  }
+    color: isTempHigh ? "red" : "blue",
+  };
   return (
     <div>
       <h1>Weather Report</h1>
-      <p>Temperature: <span style={temperatureStyle}>{weatherInput.temperature}°C</span></p>
-      <p>Conditions: <span style={temperatureStyle}>{weatherInput.conditions}</span></p>
+      <p>
+        Temperature:{" "}
+        <span style={temperatureStyle}>{weatherInput.temperature}</span>
+      </p>
+      <p>
+        Conditions: {weatherInput.conditions}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
